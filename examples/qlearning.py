cad6b69139be75ddc2ba48178a55f27b3ad4072f
Andrés Felipe Zapata Palacio <afzp99@gmail.com>
2020-10-29 10:24:58 -0500

    Q Learning Base Structure

---
+++ examples/qlearning.py
@@ -0,0 +1,26 @@
+def qLearning(env):
+    currentState = env.reset()
+    while True:
+        action = policy(currentState)               # Exploitation
+        if np.random.random() < explorationRate(i): # Exploration
+            action = env.action_space.sample()      # Explora una acción cualquiera
+
+        newState, reward, done, info = env.step(action)   # Cambiar de estado
+
+        if done:
+            print("Finished Iteration ",i)
+            break
+
+        # Update Q-Table
+        lr = learningRate(i)
+        learnt_value = updateQ(reward, newState)
+        oldValue = Qtable[currentState][action]
+        newValue = Qtable[currentState][action] = ( lr * newValue )  +  ( (1-lr) * oldValue )
+        currentState = newState
+        
+if __name__ == "__main__": 
+    env = HetNetEnv()
+    iter = 10
+    for i in range(iter):
+        qLearning(env)
+    env.close()
--- gym_hetnet/envs/hetnet_env.py
+++ gym_hetnet/envs/hetnet_env.py
@@ -4,15 +4,24 @@ from gym.utils import seeding
 
 class HetnetEnv(gym.Env):
   metadata = {'render.modes': ['human']}
+  Qtable = None
 
   def __init__(self):
-    raise NotImplementedError
+    nActions = 3
+    nStates = 10
+    self.action_space = spaces.Discrete(nActions)
+    self.observation_space = spaces.Discrete(nStates)
 
   def step(self, action):
-    raise NotImplementedError
+    observation = 0  # TODO
+    reward = 0       # TODO
+    done = True      # Supongo que nunca llega a Done o solo con 0 users
+    info = {}
+    return observation, reward, done, info
 
   def reset(self):
-    raise NotImplementedError
+    observation = self.observation_space.sample()
+    return observation
 
   def render(self, mode='human'):
     raise NotImplementedError
