da995d9b6de7596c8990e682454267d4778d5b44
Juan David Arcila Moreno <jarcil13@eafit.edu.co>
2020-11-03 12:38:59 -0500

    Added 'step' implementation. New version of q-learning

--- examples/qlearning.py
+++ examples/qlearning.py
@@ -1,28 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import gym
+import random
+
+# HYPERPARAMETERS
+train_episodes = 2000         # Total train episodes
+test_episodes = 100           # Total test episodes
+max_steps = 100               # Max steps per episode
+alpha = 0.7                   # Learning rate
+gamma = 0.618                 # Discounting rate
+
+# EXPLORATION / EXPLOITATION PARAMETERS
+epsilon = 1                   # Exploration rate
+max_epsilon = 1               # Exploration probability at start
+min_epsilon = 0.01            # Minimum exploration probability 
+decay_rate = 0.01             # Exponential decay rate for exploration prob
+
 
 def qLearning(env):
-    currentState = env.reset()
-    while True:
-        action = policy(currentState)               # Exploitation
-        if np.random.random() < explorationRate(i): # Exploration
-            action = env.action_space.sample()      # Explora una acción cualquiera
+    action_size = env.action_space.n
+    state_size = env.observation_space.n
+    print("Action space size: ", action_size)
+    print("State space size: ", state_size)
 
-        newState, reward, done, info = env.step(action)   # Cambiar de estado
+    # INITIALISE Q TABLE TO ZERO
+    Q = np.zeros((state_size, action_size))
 
-        if done:
-            print("Finished Iteration ",i)
-            break
 
-        # Update Q-Table
-        lr = learningRate(i)
-        learnt_value = updateQ(reward, newState)
-        oldValue = Qtable[currentState][action]
-        newValue = Qtable[currentState][action] = ( lr * newValue )  +  ( (1-lr) * oldValue )
-        currentState = newState
+    # TRAINING PHASE
+    training_rewards = []   # list of rewards
+
+    for episode in range(train_episodes):
+        state = env.reset()    # Reset the environment
+        cumulative_training_rewards = 0
         
+        for step in range(max_steps):
+            # Choose an action (a) among the possible states (s)
+            exp_exp_tradeoff = random.uniform(0, 1)   # choose a random number
+            
+            # If this number > epsilon, select the action corresponding to the biggest Q value for this state (Exploitation)
+            if exp_exp_tradeoff > epsilon:
+                action = np.argmax(Q[state,:])        
+            # Else choose a random action (Exploration)
+            else:
+                action = env.action_space.sample()
+            
+            # Perform the action (a) and observe the outcome state(s') and reward (r)
+            new_state, reward, done, info = env.step(action)
+
+            # Update the Q table using the Bellman equation: Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+            Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[new_state, :]) - Q[state, action]) 
+            cumulative_training_rewards += reward  # increment the cumulative reward        
+            state = new_state         # Update the state
+            
+            # If we reach the end of the episode
+            if done == True:
+                print ("Cumulative reward for episode {}: {}".format(episode, cumulative_training_rewards))
+                break
+        
+        # Reduce epsilon (because we need less and less exploration)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
+        
+        # append the episode cumulative reward to the list
+        training_rewards.append(cumulative_training_rewards)
+
+    print ("Training score over time: " + str(sum(training_rewards)/train_episodes))
+
+
 if __name__ == "__main__": 
-    env = gym.make('gym_hetnet:hetnet-v0')
-    iter = 10
-    for i in range(iter):
-        qLearning(env)
-    env.close()
+    # CREATE THE ENVIRONMENT
+    env = gym.make('gym_hetnet:hetnet-v0', MacroMaxCapacity=2, FentoMaxCapacity=1)
+    # Run Q-Learning
+    qLearning(env)
--- gym_hetnet/envs/hetnet_env.py
+++ gym_hetnet/envs/hetnet_env.py
@@ -2,21 +2,142 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+map_state = {
+  'S1'  : 0
+  'S2'  : 1
+  'S3'  : 2
+  'S4M' : 3
+  'S4f1': 4
+  'S5M' : 5
+  'S5f2': 6
+}
+
+code_event_macro_in = [0,1,2,3,5,7,9]
+code_event_macro_out = [8,10,11,12,13,,14,16]
+
 class HetnetEnv(gym.Env):
   metadata = {'render.modes': ['human']}
-  Qtable = None
 
-  def __init__(self):
-    nActions = 3
-    nStates = 10
-    self.action_space = spaces.Discrete(nActions)
-    self.observation_space = spaces.Discrete(nStates)
+  def __init__(self, MacroMaxCapacity=2, FentoMaxCapacity=1):
+    self.MacroMaxCapacity = MacroMaxCapacity
+    self.FentoMaxCapacity = FentoMaxCapacity
+    self.observation_space = spaces.MultiDiscrete([MacroMaxCapacity,MacroMaxCapacity,MacroMaxCapacity, MacroMaxCapacity, FentoMaxCapacity, MacroMAxCapacity, FentoMaxCapacity, 17])
+    self.action_space = spaces.Discrete(10)
+  
 
+  def __accept_connection_Macro():
+     state = self.observation_space.nvec
+     event = state[-1] # Last value in state is the event code
+    
+     if event in code_event_macro_in:
+       # TODO 
+       if event == 0:
+         indice = 0
+       elif event == 1:
+         indice = 1
+       elif event == 2
+       elif event == 3
+
+     else: 
+       print(f"Code event {event} does not match with accept connection...")
+     += 1
+
+  def __release_connection_Macro():
+     # TODO 
+     -= 1
+    
+ 
+  def __Macro_connections(self)
+    state = self.observation_space.nvec
+    return state[map_state['S1']] + state[map_state['S2']] + state[map_state['S3']] + state[map_state['S4M']] + state[map_state['S5M']]
+  
   def step(self, action):
-    observation = 0  # TODO
-    reward = 0       # TODO
-    done = True      # Supongo que nunca llega a Done o solo con 0 users
+    assert self.action_space.contains(action)
+    reward = 0
+    if action == 10: # If action is continue, do nothing and continue
+      return self.observation_space.nvec, reward, False, {}
+
+    elif action == 0: 
+      if self.__Macro_connections() < self.MacroMaxCapacity: # Can accept
+        self.__accept_connection_Macro()
+      else: # Can't accept connection to macro. State impossible
+        reward = -1
+
+    elif action == 1:
+      if self.observation_space.nvec[map_state['S4f1']] < self.FentoMaxCapacity # Can accept
+        self.observation_space.nvec[map_state['S4f1']] += 1
+      else:  # Can't accept connection to Fento 1 (S4). State impossible
+        reward = -1
+
+    elif action == 2:
+      if self.observation_space.nvec[map_state['S5f2']] < self.FentoMaxCapacity # Can accept
+        self.observation_space.nvec[map_state['S5f2']] += 1
+      else:  # Can't accept connection to Fento 2 (S5). State impossible
+        reward = -1
+
+    elif action == 3:
+      if self.__Macro_connections() > 0: # Can release connection
+        self.__release_connection_Macro()
+      else: # Can't release connection if Macro is empty. State impossible
+        reward = -1
+
+    elif action == 4:
+      if self.observation_space.nvec[map_state['S4f1']] > 0: # Can release connection
+        self.observation_space.nvec[map_state['S4f1']] -= 1
+      else:  # Can't release connection if Fento 1 (S4) is empty. State impossible
+        reward = -1
+
+    elif action == 5:
+      if self.observation_space.nvec[map_state['S5f2']] > 0: # Can release connection
+        self.observation_space.nvec[map_state['S5f2']] -= 1
+      else:  # Can't release connection if Fento 2 (S5) is empty. State impossible
+        reward = -1
+
+    elif action == 6:
+       # Check if can handoff a connection form Macro to S4f1
+       if self.__Macro_connections() > 0 and self.observation_space.nvec[map_state['S4f1']] < self.FentoMaxCapacity
+          reward = 10
+          self.__release_connection_Macro()
+          self.observation_space.nvec[map_state['S4f1']] += 1
+       else: # Impossible to do handoff
+         reward = -1
+
+    elif action == 7:
+      # Check if can handoff a connection form Macro to S5f2
+       if self.__Macro_connections() > 0 and self.observation_space.nvec[map_state['S5f2']] < self.FentoMaxCapacity
+          reward = 10
+          self.__release_connection_Macro()
+          self.observation_space.nvec[map_state['S5f2']] += 1
+       else: # Impossible to do handoff
+         reward = -1
+
+    elif action == 8:
+      # Check if can hadoff a connection from S4f1 to Macro
+      if self.observation_space.nvec[map_state['S4f1']] > 0 and self.__Macro_connections() < self.MacroMaxCapacity:
+        reward = 10
+        self.__accept_connection_Macro()
+        self.observation_space.nvec[map_state['S4f1']] -= 1
+      else: #  Impossible to do handoff
+        reward = -1
+
+    elif action == 9:
+      # Check if can hadoff a connection from S4f1 to Macro
+      if self.observation_space.nvec[map_state['S5f2']] > 0 and self.__Macro_connections() < self.MacroMaxCapacity:
+        reward = 10
+        self.__accept_connection_Macro()
+        self.observation_space.nvec[map_state['S5f2']] -= 1
+      else: #  Impossible to do handoff
+        reward = -1
+    
+    # This statement should be unreacheable.
+    else:  # But, just in case :D
+      raise Exception(f"Action: {action} not defined")
+    
+    # Finally, we used the observation space with the changes made by the action
+    observation = self.observation_space.nvec
+    done = False # Stop the simulation is not implemented yet :D 
     info = {}
+    # And return everything
     return observation, reward, done, info
 
   def reset(self):
